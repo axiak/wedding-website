a6a8d66d46a0f498ed3281561e2983e15b56db16
Mike Axiak <maxiak@hubspot.com>
2013-02-04 23:15:43 -0500

    Remove build

--- pybackend/build/sqlalchemy/lib/sqlalchemy/testing/suite/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from sqlalchemy.testing.suite.test_ddl import *
-from sqlalchemy.testing.suite.test_insert import *
-from sqlalchemy.testing.suite.test_update_delete import *
-from sqlalchemy.testing.suite.test_reflection import *
-from sqlalchemy.testing.suite.test_types import *
